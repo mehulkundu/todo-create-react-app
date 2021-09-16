463e6f548fdbe3e5ba0387b09497d4adfb1d82dd
kundu <programmer.mehul@gmail.com>
2021-09-16 21:25:39 +0530

    propTypes defaultProps inline CSS

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,35 @@
-const Header = (props) => {
+import PropTypes from 'prop-types';
+
+const Header = ({ title }) => {
     return (
         <header>
-            <h1>{props.title}</h1>
+            <h1>{title}</h1>
         </header>
     )
 }
+
+//Object create method of passing props
+// const Header = (props) => {
+//     return (
+//         <header>
+//             <h1>{props.title}</h1>
+//         </header>
+//     )
+// }
+
+Header.defaultProps = {
+    title: 'Task Tracker',
+}
+
+//Defining propTypes
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+//CSS in JS
+// const HeadingStyle = {
+//     color: 'red',
+//     backgroudColor: 'blue',
+// }
 
 export default Header
